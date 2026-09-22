9de7e44bb36c78dfc9e3aa1862edb473ce733a22
WhiteByte <animationr86@gmail.com>
2026-09-23 00:22:52 +0330

    Use server.py as the canonical PixelLink server

--- server.py
+++ server.py
@@ -9,34 +9,26 @@ HEIGHT = 128
 
 sct = mss.mss()
 
+
 @app.route("/frame")
 def frame():
-
     monitor = sct.monitors[1]
-
     shot = sct.grab(monitor)
 
-    img = Image.frombytes("RGB", shot.size, shot.rgb)
+    image = Image.frombytes("RGB", shot.size, shot.rgb)
+    image = image.resize((WIDTH, HEIGHT), Image.Resampling.LANCZOS)
 
-    img = img.resize((WIDTH, HEIGHT), Image.Resampling.LANCZOS)
-
-    pixels = img.load()
-
+    pixels = image.load()
     rgb565 = bytearray()
 
     for y in range(HEIGHT):
         for x in range(WIDTH):
             r, g, b = pixels[x, y]
-
             color = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3)
-
             rgb565.append(color >> 8)
             rgb565.append(color & 0xFF)
 
-    return Response(
-        bytes(rgb565),
-        mimetype="application/octet-stream"
-    )
+    return Response(bytes(rgb565), mimetype="application/octet-stream")
 
 
 if __name__ == "__main__":
